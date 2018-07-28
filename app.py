78e609a47be64a1b795b87e386bb753a14523de4
Tanja Nyberg <32233040+TanjaNY@users.noreply.github.com>
2018-07-28 12:44:46 -0400

    Update app.py

--- app.py
+++ app.py
@@ -61,7 +61,7 @@ def scrape():
      # Run scraped functions
     
     
-    url="https://tanjany.github.io/GWU-Bootcamp-Project02/"
+    url="https://tanjany.github.io/GWU-Bootcamp-Project02/index.html"
     
      # Redirect back to home page 
     return redirect(url, code=302)

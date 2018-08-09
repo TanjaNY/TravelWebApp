39df59801e58598a383b076744e3fd327c7d9e7a
Tanja Nyberg <32233040+TanjaNY@users.noreply.github.com>
2018-08-09 13:40:55 -0400

    Update app.py

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ collection = db.tweets
 
 
 
-db.tweets.drop()
+#db.tweets.drop()
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)

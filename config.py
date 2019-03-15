fd933102aaa4f3bfe2794f1e1bd881d2e5c6accb
Tanja Nyberg <32233040+TanjaNY@users.noreply.github.com>
2019-03-15 19:35:25 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-consumer_key="W7eaDLpDVy8TjhpLioP2gddED"
-consumer_secret="9twochzAihnsaMTWwbEyICwkVHGInkFgsiXadSG4p5jskWba1Y"
+consumer_key="GmJPV7YJA0YzfnBmPDg8pkVMW"
+consumer_secret="ln2sMygoZhBQlvNAjv3GShpWC6lGIleDxie7HBwTN9HGPbbgym"
 access_token="987885617043464192-f7jBC47kNQ4gGcMyLBzRv9sD5GLC13R"
 access_token_secret="kNASDSEaz7zp5yL0FjdgNs04CsQgM0GmqoWmsZidAxWmA"

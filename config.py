08b21ecfbabe97552a743af33befd8a6bd479a38
Tanja Nyberg <32233040+TanjaNY@users.noreply.github.com>
2020-08-23 13:47:07 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 
-consumer_key="GmJPV7YJA0YzfnBmPDg8pkVMW"
-consumer_secret="ln2sMygoZhBQlvNAjv3GShpWC6lGIleDxie7HBwTN9HGPbbgym"
-access_token="987885617043464192-f7jBC47kNQ4gGcMyLBzRv9sD5GLC13R"
-access_token_secret="kNASDSEaz7zp5yL0FjdgNs04CsQgM0GmqoWmsZidAxWmA"
-HOST = 'ds113826.mlab.com'
-PORT = 13826
-database_user = 'celasson'
-database_pass = 'Yorktown2008'
-databasename ='heroku_g12ldwwq'
+consumer_key="xxxx"
+consumer_secret="xxx"
+access_token="xxx"
+access_token_secret="xxxx"
+HOST = 'xxx'
+PORT = xxxx
+database_user = 'xxxx'
+database_pass = 'xxxx'
+databasename ='xxxx'
